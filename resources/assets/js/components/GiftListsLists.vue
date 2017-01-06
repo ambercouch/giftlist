2656c649742c2ce474853456dc39eb8c0f7fea75
richiearnold <richard@ambercouch.co.uk>
2017-01-06 20:01:57 +0000

    added url to giftlist vue template

--- resources/assets/js/components/GiftListsLists.vue
+++ resources/assets/js/components/GiftListsLists.vue
@@ -2,10 +2,10 @@
     <div class="panel panel-default">
         <div class="panel-heading"><h2>Your Gift lists</h2></div>
         <div class="panel-body">
-            <p>Click a list to view or edit your gifts or create a new list of gifts to share with your friends and family</p>
+            <p>123 Click a list to view or edit your gifts or create a new list of gifts to share with your friends and family</p>
 
             <ul>
-                <li v-for="list in giftlists">{{ list.gift_list_name}}</li>
+              <li v-for="list in giftlists"><a :href="'api/giftlists/'+list.id">{{ list.gift_list_name}}</a></li>
             </ul>
         </div>
     </div>
