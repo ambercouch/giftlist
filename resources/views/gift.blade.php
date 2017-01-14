1bd0adbd548e20c3c279fe2636cd62a6cafd338b
richiearnold <richard@ambercouch.co.uk>
2017-01-14 16:21:15 +0000

    added single gift view

---
+++ resources/views/gift.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+           <giftlist listid="{{$id}}">
+        </div>
+    </div>
+</div>
+
+@endsection
