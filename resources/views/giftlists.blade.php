6fab4502be7eabec46f02aa6304b2540c0148b3e
richiearnold <richard@ambercouch.co.uk>
2017-02-06 20:50:36 +0000

    added flash messages to giftlists

--- resources/views/giftlists.blade.php
+++ resources/views/giftlists.blade.php
@@ -4,6 +4,8 @@
     @verbatim
 
 <div class="container">
+    <flashmessage></flashmessage>
+
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <GiftListList v-bind:giftlists="shared.state.giftlists" v-on:updategiftlists="updateGiftLists"></GiftListList>
